883d61d9fb154d95fc40d18b5269e457daf5e3a6
Anantha Thyagarajan <athyagarajan@zipwhip.com>
2020-08-05 09:03:07 -0700

    first ios app commit

---
+++ FoodTracker/FoodTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTracker
+//
+//  Created by Anantha Thygarajan on 8/4/20.
+//  Copyright © 2020 Zipwhip. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Zipwhip!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# hellozw-ios-app
